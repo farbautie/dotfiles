d74b53a7b9736739904bbd90bc4937fdec959a1b
Camilo Vargas <farbautie@Farbautie>
2024-06-07 20:42:37 -0500

    feat: basic nvim config with lazyvim

---
+++ README.md
@@ -0,0 +1,33 @@
+# Dotfiles
+
+These are my configuration files (dotfiles) to customize my development environment
+with LazyVim, Zellij, LazyGit, and Fish.
+These files contain settings and configurations
+that I've found useful for my daily workflow.
+
+## Contents
+
+- **LazyVim**: Configuration for the Vim text editor.
+- **Zellij**: Configuration for the terminal multiplexer Zellij.
+- **LazyGit**: Configuration for the Git user interface, LazyGit.
+- **Fish**: Configuration for the Fish shell.
+
+## Installation
+
+- Clone this repository to your local machine
+
+```bash
+git clone https://github.com/farbautie/dotfiles
+```
+
+- Copy the files to your home directory
+
+```bash
+cp -r dotfiles ~/.config
+```
+
+## Customization
+
+Feel free to customize these files according to your own preferences and needs.
+You can modify colors, keyboard shortcuts, plugins, and other configurations as you wish.
+Remember to keep a backup of your previous configurations before making significant changes.
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,44 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local pickers = require("huez.pickers")
+
+vim.keymap.set("n", "<leader>co", pickers.themes, {})
+vim.keymap.set("n", "<leader>cof", pickers.favorites, {})
+vim.keymap.set("n", "<leader>col", pickers.live, {})
+vim.keymap.set("n", "<leader>coe", pickers.ensured, {})
+
+vim.keymap.set("i", "<C-d>", "<C-d>zz")
+vim.keymap.set("i", "<C-u>", "<C-u>zz")
+vim.keymap.set("i", "<C-b>", "<C-o>de")
+
+-- REQUIRED
+local oil = require("oil")
+oil.setup()
+-- REQUIRED
+
+-----  OIL -----
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Delete all buffers but the current one --
+vim.keymap.set(
+  "n",
+  "<leader>bq",
+  '<Esc>:%bdelete|edit #|normal`"<Return>',
+  { desc = "Delete other buffers but the current one" }
+)
+
+-- Disable key mappings in insert mode
+vim.api.nvim_set_keymap("i", "<A-j>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-k>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable key mappings in normal mode
+vim.api.nvim_set_keymap("n", "<A-j>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-k>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable key mappings in visual block mode
+vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "K", "<Nop>", { noremap = true, silent = true })
